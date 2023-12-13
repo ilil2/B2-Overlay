33f4f89476c3c2f8e2081e4d823525a6d13ccedd
HIRTH Nathan <73468478+Narth42@users.noreply.github.com>
2023-12-13 18:47:21 +0100

    Update du script de l'overlay

--- overlay.js
+++ overlay.js
@@ -9,46 +9,17 @@
 // @updateURL    https://github.com/ilil2/B2-Overlay/blob/main/overlay.js
 // @downloadURL  https://github.com/ilil2/B2-Overlay/blob/main/overlay.js
 // ==/UserScript==
+(function() {
+    'use strict';
 
-// options
-let OVERLAY_IMAGE = "https://github.com/ilil2/B2-Overlay/blob/main/image.png";
-let CANVAS_SIZE = [255, 255];
-let MIN_ZOOM_LEVEL = 0.2; // minimum zoom level to display overlay
+    // URL de votre image (doit être hébergée en ligne)
+    const imageUrl = "https://github.com/ilil2/B2-Overlay/blob/main/image.png";
 
-// create the overlay image
-const overlayImage = new Image();
-const overlayStyles = {
-	position: "absolute",
-	imageRendering: "pixelated",
-	width: CANVAS_SIZE[0] + "px",
-	height: CANVAS_SIZE[1] + "px",
-	pointerEvents: "none",
-};
+    // Créer un élément d'image et le configurer
+    const overlayImage = new Image();
+    overlayImage.src = imageUrl;
+    overlayImage.style = 'position: absolute; left: 0; top: 0; z-index: 1000; opacity: 0.5;';
 
-for (let rule in overlayStyles) overlayImage.style[rule] = overlayStyles[rule];
-overlayImage.src = OVERLAY_IMAGE;
-
-// make the overlay togglable
-let displayOverlay = true;
-document.addEventListener("keypress", (e) => {
-	if (e.code.toLowerCase() != "space") return;
-	displayOverlay = !displayOverlay;
-	overlayImage.style.display = displayOverlay ? "block" : "none";
-});
-
-window.addEventListener("load", () => {
-	// add the overlay to the page
-	let overlayParent = document.querySelector("#canvparent1"); // hijacking the snoo for our overlay
-
-	overlayParent.style.zIndex = "4";
-	overlayParent.append(overlayImage);
-
-	// intercept position/scale changes
-	let oldPos = pos;
-	pos = () => {
-		oldPos(); // do the normal things
-
-		if (localStorage.z < MIN_ZOOM_LEVEL / 5) overlayImage.style.display = "none";
-		else if (displayOverlay) overlayImage.style.display = "block";
-	};
-});
+    // Ajouter l'image au corps de la page
+    document.body.appendChild(overlayImage);
+})();
