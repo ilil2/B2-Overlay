3a024cf8a8d735d9deca398d80cb74d9a751c593
HIRTH Nathan <73468478+Narth42@users.noreply.github.com>
2023-12-14 17:10:56 +0100

    Create WG-Overlay-0.1.user.js

---
+++ WG-Overlay-0.1.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         B2 Overlay
+// @namespace    https://github.com/ilil2/B2-Overlay
+// @version      0.1
+// @description  WIN AGAINST D2
+// @author       WarpGates Studio
+// @match        https://liste.bdekraken.fr/place
+// @grant        none
+// @icon         https://raw.githubusercontent.com/ilil2/B2-Overlay/main/image.png
+// @updateURL    https://raw.githubusercontent.com/ilil2/B2-Overlay/main/WG-Overlay-0.1.user.js
+// @downloadURL  https://raw.githubusercontent.com/ilil2/B2-Overlay/main/WG-Overlay-0.1.user.js
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    function setupImage() {
+        if (window.location.href === 'https://liste.bdekraken.fr/place') {
+
+            var canvas = document.getElementsByClassName("canvas svelte-1wet9f1");
+
+            if (true) {
+                var context = canvas[0].getContext('2d');
+
+                var image = new Image();
+                image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/pixelwarpgates.png';
+
+                image.onload = function() {
+                    var x = (4080/255)*33;
+                    var y = (4080/255)*247;
+
+                    var newWidth = (4080/255)*59; 
+                    var newHeight = (newWidth / image.width) * image.height; 
+
+                    context.globalAlpha = '1'
+
+                    context.drawImage(image, x, y, newWidth, newHeight);
+                };
+            }
+        }
+    }
+    setTimeout(function() {
+        setupImage();
+    }, 5000);
+    setInterval(function() {
+        setupImage();
+    }, 10);
+})();
