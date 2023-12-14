64301ca54212ab2d6d405eae1cdc9639d0f57cdc
ilil2 <108801514+ilil2@users.noreply.github.com>
2023-12-14 14:06:49 +0100

    Final2

--- B2-Overlay-0.1.user.js
+++ B2-Overlay-0.1.user.js
@@ -16,24 +16,22 @@
     'use strict';
 
     function setupImage() {
-        // Vérification pour s'assurer que nous sommes sur le bon site
         if (window.location.href === 'https://liste.bdekraken.fr/place') {
 
-            // Récupération du canvas par son ID (remplacez "nom-du-canvas" par l'ID réel de votre canvas)
             var canvas = document.getElementsByClassName("canvas svelte-1wet9f1");
 
             if (true) {
                 var context = canvas[0].getContext('2d');
 
                 var image = new Image();
-                image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/OverlayB2.png'; // Remplacez par le chemin de votre image
+                image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/OverlayB2.png';
 
                 image.onload = function() {
                     var x = (4080/255)*153;
                     var y = (4080/255)*5;
 
-                    var newWidth = (4080/255)*102; // Ajustez la nouvelle largeur selon vos besoins
-                    var newHeight = (newWidth / image.width) * image.height; // Maintenir le ratio
+                    var newWidth = (4080/255)*102; 
+                    var newHeight = (newWidth / image.width) * image.height; 
 
                     context.globalAlpha = '1'
 
@@ -42,12 +40,10 @@
             }
         }
     }
-    console.log('2')
     setTimeout(function() {
         setupImage();
     }, 5000);
-
-    while (true){
+    setInterval(function() {
         setupImage();
-    }
+    }, 10);
 })();
