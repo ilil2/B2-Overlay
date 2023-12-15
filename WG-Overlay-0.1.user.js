f1c1cbf330e9860f7e1c5e31d82893cbeb302d63
ilil2 <108801514+ilil2@users.noreply.github.com>
2023-12-15 16:42:22 +0100

    Update kraken

--- WG-Overlay-0.1.user.js
+++ WG-Overlay-0.1.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         WG Overlay
+// @name         WB Overlay
 // @namespace    https://github.com/ilil2/B2-Overlay
 // @version      0.1
 // @description  WIN AGAINST D2
 // @author       WarpGates Studio
 // @match        https://liste.bdekraken.fr/place
 // @grant        none
-// @icon         https://raw.githubusercontent.com/ilil2/B2-Overlay/main/pixelwarpgates-2.png
+// @icon         https://raw.githubusercontent.com/ilil2/B2-Overlay/main/image.png
 // @updateURL    https://raw.githubusercontent.com/ilil2/B2-Overlay/main/WG-Overlay-0.1.user.js
 // @downloadURL  https://raw.githubusercontent.com/ilil2/B2-Overlay/main/WG-Overlay-0.1.user.js
 // ==/UserScript==
@@ -25,17 +25,26 @@
 
                 var image = new Image();
                 image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/pixelwarpgates.png';
+                var image2 = new Image();
+                image2.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/Overlay-Kraken.png';
 
                 image.onload = function() {
                     var x = (4080/255)*33;
                     var y = (4080/255)*247;
 
-                    var newWidth = (4080/255)*59; 
-                    var newHeight = (newWidth / image.width) * image.height; 
+                    var x2 = (4080/255)*132;
+                    var y2 = (4080/255)*242;
+
+                    var newWidth = (4080/255)*59;
+                    var newHeight = (newWidth / image.width) * image.height;
+
+                    var newWidth2 = (4080/255)*26;
+                    var newHeight2 = (newWidth2 / image2.width) * image2.height;
 
                     context.globalAlpha = '1'
 
                     context.drawImage(image, x, y, newWidth, newHeight);
+                    context.drawImage(image2, x2, y2, newWidth2, newHeight2);
                 };
             }
         }
