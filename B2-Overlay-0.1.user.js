331bdb0f85fee48f9a55d70137867d24fc4f561a
HIRTH Nathan <73468478+Narth42@users.noreply.github.com>
2023-12-18 21:09:53 +0100

    Update B2-Overlay-0.1.user.js

--- B2-Overlay-0.1.user.js
+++ B2-Overlay-0.1.user.js
@@ -5,6 +5,7 @@
 // @description  WIN AGAINST D2
 // @author       WarpGates Studio
 // @match        https://liste.bdekraken.fr/place
+// @match        https://liste.bdekraken.fr/place/
 // @grant        none
 // @icon         https://raw.githubusercontent.com/ilil2/B2-Overlay/main/image.png
 // @updateURL    https://raw.githubusercontent.com/ilil2/B2-Overlay/main/B2-Overlay-0.1.user.js
@@ -25,17 +26,24 @@
 
                 var image = new Image();
                 image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/OverlayB2.png';
+                var image2 = new Image();
+                image2.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/kraken-3px.png';
 
                 image.onload = function() {
                     var x = (4080/255)*0;
                     var y = (4080/255)*5;
+                    var x2 = (4080/255)*20;
+                    var y2 = (4080/255)*150;
 
-                    var newWidth = (4080/255)*255; 
-                    var newHeight = (newWidth / image.width) * image.height; 
+                    var newWidth = (4080/255)*255;
+                    var newHeight = (newWidth / image.width) * image.height;
+                    var newWidth2 = (4080/255)*68;
+                    var newHeight2 = (newWidth2 / image2.width) * image2.height;
 
                     context.globalAlpha = '1'
 
                     context.drawImage(image, x, y, newWidth, newHeight);
+                    context.drawImage(image2, x2, y2, newWidth2, newHeight2);
                 };
             }
         }
