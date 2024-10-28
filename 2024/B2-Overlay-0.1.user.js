6a632ff22183d9beaa8f0f7e55ef21a8694a1901
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2024-10-28 10:59:20 +0100

    changemen canva 2024

--- 2024/B2-Overlay-0.1.user.js
+++ 2024/B2-Overlay-0.1.user.js
@@ -7,9 +7,9 @@
 // @match        https://liste.bdekraken.fr/place
 // @match        https://liste.bdekraken.fr/place/
 // @grant        none
-// @icon         https://raw.githubusercontent.com/ilil2/B2-Overlay/main/image.png
-// @updateURL    https://raw.githubusercontent.com/ilil2/B2-Overlay/main/B2-Overlay-0.1.user.js
-// @downloadURL  https://raw.githubusercontent.com/ilil2/B2-Overlay/main/B2-Overlay-0.1.user.js
+// @icon         https://raw.githubusercontent.com/ilil2/B2-Overlay/main/2024/image.png
+// @updateURL    https://raw.githubusercontent.com/ilil2/B2-Overlay/main/2024/B2-Overlay-0.1.user.js
+// @downloadURL  https://raw.githubusercontent.com/ilil2/B2-Overlay/main/2024/B2-Overlay-0.1.user.js
 // ==/UserScript==
 
 
@@ -19,31 +19,31 @@
     function setupImage() {
         if (window.location.href === 'https://liste.bdekraken.fr/place') {
 
-            var canvas = document.getElementsByClassName("canvas svelte-1wet9f1");
+            var canvas = document.getElementsByClassName("canvas svelte-1kjn625");
 
             if (true) {
                 var context = canvas[0].getContext('2d');
 
                 var image = new Image();
-                image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/OverlayB2.png';
-                var image2 = new Image();
-                image2.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/kraken-3px.png';
+                image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/2024/OverlayB2.png';
+                //var image2 = new Image();
+                //image2.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/kraken-3px.png';
 
                 image.onload = function() {
                     var x = (4080/255)*0;
                     var y = (4080/255)*5;
-                    var x2 = (4080/255)*20;
-                    var y2 = (4080/255)*150;
+                    //var x2 = (4080/255)*20;
+                    //var y2 = (4080/255)*150;
 
                     var newWidth = (4080/255)*255;
                     var newHeight = (newWidth / image.width) * image.height;
-                    var newWidth2 = (4080/255)*68;
-                    var newHeight2 = (newWidth2 / image2.width) * image2.height;
+                    //var newWidth2 = (4080/255)*68;
+                    //var newHeight2 = (newWidth2 / image2.width) * image2.height;
 
                     context.globalAlpha = '1'
 
                     context.drawImage(image, x, y, newWidth, newHeight);
-                    context.drawImage(image2, x2, y2, newWidth2, newHeight2);
+                    //context.drawImage(image2, x2, y2, newWidth2, newHeight2);
                 };
             }
         }
