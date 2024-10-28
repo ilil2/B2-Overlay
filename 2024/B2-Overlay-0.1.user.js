06ee29a34e56e4b437fad32cbef1f4c06753ae62
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2024-10-28 14:22:14 +0100

    rick

--- 2024/B2-Overlay-0.1.user.js
+++ 2024/B2-Overlay-0.1.user.js
@@ -26,24 +26,24 @@
 
                 var image = new Image();
                 image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/2024/r2b2-3px.png';
-                //var image2 = new Image();
-                //image2.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/kraken-3px.png';
+                var image2 = new Image();
+                image2.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/rick-3px.png';
 
                 image.onload = function() {
                     var x = (4080/255)*218;
                     var y = (4080/255)*208;
-                    //var x2 = (4080/255)*20;
-                    //var y2 = (4080/255)*150;
+                    var x2 = (4080/255)*100;
+                    var y2 = (4080/255)*100;
 
                     var newWidth = (4080/255)*22;
                     var newHeight = (newWidth / image.width) * image.height;
-                    //var newWidth2 = (4080/255)*68;
-                    //var newHeight2 = (newWidth2 / image2.width) * image2.height;
+                    var newWidth2 = (4080/255)*47;
+                    var newHeight2 = (newWidth2 / image2.width) * image2.height;
 
                     context.globalAlpha = '1'
 
                     context.drawImage(image, x, y, newWidth, newHeight);
-                    //context.drawImage(image2, x2, y2, newWidth2, newHeight2);
+                    context.drawImage(image2, x2, y2, newWidth2, newHeight2);
                 };
             }
         }
