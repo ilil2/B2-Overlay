dc36788358fb59161303253b7fc74cd286ab6bc4
Ilyann gwinner <ilyann.gwinner@epita.fr>
2026-05-15 10:49:20 +0200

    add: new process image
    Add argv in process_image.py

---
+++ process_image.py
@@ -0,0 +1,26 @@
+import sys
+from PIL import Image
+
+def agrandir_image(input_path, output_path):
+    with Image.open(input_path) as img:
+        largeur, hauteur = img.size
+
+        nouvelle_largeur, nouvelle_hauteur = largeur * 3, hauteur * 3
+        nouvelle_image = Image.new('RGBA', (nouvelle_largeur, nouvelle_hauteur), color = (0,0,0,0))
+
+        pixels = img.load()
+        for y in range(hauteur):
+            for x in range(largeur):
+                rgba = pixels[x, y]
+                nouvelle_image.putpixel((3*x + 1, 3*y + 1), rgba)
+
+        nouvelle_image.save(output_path, format = "PNG")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python process_image.py [image]");
+    else:
+        img_name = sys.argv[1]
+        new_img_name = "-3px.".join(img_name.split('.'))
+        agrandir_image(img_name, new_img_name)
