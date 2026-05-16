437ad5b4d2fc781a619ee4e400745df4a2c936b3
Ilyann gwinner <ilyann.gwinner@epita.fr>
2026-05-16 12:42:00 +0200

    add: epiplace

---
+++ epiplace/overlay.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Epiplace Overlay
+// @namespace    https://github.com/ilil2/B2-Overlay
+// @version      1.0
+// @description  Ban?
+// @author       WarpGates Studio
+// @match        http://localhost:8080/*
+// @grant        none
+// @icon         https://raw.githubusercontent.com/ilil2/B2-Overlay/main/2024/image.png
+// @updateURL    https://raw.githubusercontent.com/ilil2/B2-Overlay/main/epiplace/overlay.js
+// @downloadURL  https://raw.githubusercontent.com/ilil2/B2-Overlay/main/epiplace/overlay.js
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+
+    function setupImage() {
+        var canvas = document.getElementsByClassName("Canvas");
+
+        if (true) {
+            var context = canvas[0].getContext('2d');
+
+            var image = new Image();
+            image.src = 'https://raw.githubusercontent.com/ilil2/B2-Overlay/main/epiplace/theophile-johnson-3px.png';
+
+            image.onload = function () {
+                var x = (4080 / 250) * 27;
+                var y = (4080 / 250) * 34;
+
+                var newWidth = (4080 / 250) * 20;
+                var newHeight = (newWidth / image.width) * image.height;
+
+                context.globalAlpha = '1';
+
+                context.drawImage(image, x, y, newWidth, newHeight);
+            };
+        }
+    }
+    setTimeout(function () {
+        setupImage();
+    }, 2500);
+    setInterval(function () {
+        setupImage();
+    }, 10);
+})();
